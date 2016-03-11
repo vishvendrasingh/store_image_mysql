6b1819cc93b22dfac09909bdc04fd163168836f3
Vishvendra Singh <vishvendrasingh1993@gmail.com>
2016-03-11 17:17:40 +0530

    with mysqli

---
+++ index_with_mysqli.php
@@ -0,0 +1,20 @@
+<?php
+	//to upload the image from any link
+	$conn = mysqli_connect('localhost','root','','test');
+	if(!$conn) { die('could not connect!'); }
+	$image = file_get_contents('http://www.keenthemes.com/preview/conquer/assets/plugins/jcrop/demos/demo_files/image1.jpg');
+	$image = $conn->real_escape_string($image);
+	$q = mysqli_query($conn,"INSERT INTO `testblob`(`image`) VALUES('$image')");
+	if($q) echo 'image stored successfully';
+
+	//to see the image
+	$conn = mysqli_connect('localhost','root','','test');
+	if(!$conn) { die('could not connect!'); }
+	$q = mysqli_query($conn,"SELECT * FROM `testblob` WHERE `image_id`='4'");
+	$r = 	mysqli_fetch_assoc($q);
+	$img = $r["image"];
+	header("Content-type: image/png");
+	mysqli_close($conn);
+
+?>
+<img src="data:image/jpeg;base64,<?php echo base64_encode( $img ); ?>" />
